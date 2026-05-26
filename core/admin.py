3cd05942fa02bd774e6950b405c56112a8f5a592
Muhammad Hassan <mehassan3454@gmail.com>
2026-05-26 20:42:36 +0500

    Registered Models for admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.site.register(models.Patient)
+admin.site.register(models.Doctor)
+admin.site.register(models.Appointment)
+admin.site.register(models.Medication)
+admin.site.register(models.Prescription)
+admin.site.register(models.PrescriptionMedication)
+admin.site.register(models.Billing)
