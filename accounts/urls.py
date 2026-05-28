eea8fe79dc3eb94c2837daaa14027d04316525c6
Muhammad Hassan <mehassan3454@gmail.com>
2026-05-28 18:05:22 +0500

    Added userform, mapped view to url and edited a template

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
+app_name = "accounts"
 urlpatterns = [
-    
+    path("login/", views.user_login, name="login")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from .forms import UserForm
 # Create your views here.
+
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect("core:index")
+    if request.method == 'POST':
+        return
+    return render(request)
+
+def user_logout(request):
+    pass    
