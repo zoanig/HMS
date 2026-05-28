8b79cc4826774ca43a80fd7f6f877c7fa53ebfdf
Muhammad Hassan <mehassan3454@gmail.com>
2026-05-28 21:40:28 +0500

    Signup and login forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,31 @@
-from django.forms import ModelForm
+from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm, UsernameField
 
-class UserForm(ModelForm):
+
+class SignUPForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ["username", "password"]
+        widgets = {
+            "username": forms.TextInput(attrs={
+                "class": "input",
+                "placeholder": "Type here",
+                }),
+            "password": forms.PasswordInput(attrs={
+                    "class": "input",
+                    "placeholder": "Type here",
+                }),
+        }
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+
+    username = UsernameField(label="Username", widget=forms.TextInput(attrs={"autofocus": True, "class": "input"}))
+    password = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "current-password", "class": "input"}),
+    )
+    
