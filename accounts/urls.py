358263c22f50f415c7b62d0e3375db197fd084a3
Muhammad Hassan <mehassan3454@gmail.com>
2026-05-28 21:40:45 +0500

    mapping

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = "accounts"
 urlpatterns = [
-    path("login/", views.user_login, name="login")
+    path("login/", views.user_login, name="login"),
+    path("logout/", views.user_logout, name="logout")
 ]
