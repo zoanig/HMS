207b095426801771b3f04a5eaa7ae2edc3b4cbe4
Muhammad Hassan <mehassan3454@gmail.com>
2026-05-30 12:24:09 +0500

    Patient dashboard, partial

--- patients/urls.py
+++ patients/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
+app_name = "patients"
 
 urlpatterns = [
-    
+    path("dashboard/", views.dashboard, name='dashboard')
 ]
--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.models import Patient, Appointment
 
 # Create your views here.
+
+def dashboard(request):
+    if not request.user.is_authenticated:
+        return redirect("core:index")
+    patient = Patient.objects.get(user = request.user)
+    if patient is None:
+        return redirect("core:index")
+    appointments = Appointment.objects.filter(patient = patient.id)
+    context = {"appointments": appointments}
+    return render(request, "dashboard.html", context)
