21050b453d20f3521e9942b65baefc1447a19399
Muhammad Hassan <mehassan3454@gmail.com>
2026-05-30 17:51:44 +0500

    Dashboard functionality v1.0

--- patients/views.py
+++ patients/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect
-from core.models import Patient, Appointment
+from core.models import Patient, Appointment, Billing, PrescriptionMedication
 
 # Create your views here.
 
 def dashboard(request):
     if not request.user.is_authenticated:
         return redirect("core:index")
-    patient = Patient.objects.get(user = request.user)
-    if patient is None:
+    patientExists = Patient.objects.filter(user = request.user).exists()
+    if patientExists is False:
         return redirect("core:index")
-    appointments = Appointment.objects.filter(patient = patient.id)
-    context = {"appointments": appointments}
+    patient = Patient.objects.get(user = request.user)
+    appointments = patient.appointment_set.order_by('-appointment_date')[:3]
+    billings = Billing.objects.filter(patient = patient.id)[:1]
+    pms = PrescriptionMedication.objects.filter(prescription__patient = patient.id )
+    latest_prescription = patient.prescription_set.order_by('-created_at').first()
+    context = {"appointments": appointments, "billings": billings, "pms": pms, "latest_prescription": latest_prescription}
     return render(request, "dashboard.html", context)
