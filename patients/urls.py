6954c903ca64e0a3bee06cd10e008ad602aa7d5b
Muhammad Hassan <mehassan3454@gmail.com>
2026-06-01 12:13:36 +0500

    Get appointments by id

--- patients/urls.py
+++ patients/urls.py
@@ -3,5 +3,6 @@ from . import views
 app_name = "patients"
 
 urlpatterns = [
-    path("dashboard/", views.dashboard, name='dashboard')
+    path("dashboard/", views.dashboard, name='dashboard'),
+    path("appointment/<int:pk>", views.get_appointment, name='appointment')
 ]
--- patients/views.py
+++ patients/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from core.models import Patient, Appointment, Billing, PrescriptionMedication
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
@@ -10,9 +11,27 @@ def dashboard(request):
     if patientExists is False:
         return redirect("core:index")
     patient = Patient.objects.get(user = request.user)
-    appointments = patient.appointment_set.order_by('-appointment_date')[:3]
+    appointments = patient.appointment_set.order_by('-appointment_date')
+    total_appointments = len(appointments)
     billings = Billing.objects.filter(patient = patient.id)[:1]
     pms = PrescriptionMedication.objects.filter(prescription__patient = patient.id )
     latest_prescription = patient.prescription_set.order_by('-created_at').first()
-    context = {"appointments": appointments, "billings": billings, "pms": pms, "latest_prescription": latest_prescription}
+    context = {"appointments": appointments[:2], "billings": billings, "pms": pms, "latest_prescription": latest_prescription, "total_appointments": total_appointments}
     return render(request, "dashboard.html", context)
+
+def get_appointment(request, pk):
+    if not request.user.is_authenticated:
+        return redirect("core:index")
+    patientExists = Patient.objects.filter(user = request.user).exists()
+    if patientExists is False:
+        return redirect("core:index")
+    patient = Patient.objects.get(user = request.user)
+    apt = ""
+    try:
+        apt = patient.appointment_set.get(id=pk)
+        print(apt)
+    except ObjectDoesNotExist:
+        return render(request, "core/404.html", {"message": "The resource you are trying to access doesnt. Or you're unauthorized to access it"})
+    
+    context = {"appointment": apt}
+    return 
