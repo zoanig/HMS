81bd280a1261282f15786d1f634b7847032c0541
Muhammad Hassan <mehassan3454@gmail.com>
2026-06-13 06:35:58 +0500

    a

--- core/admin.py
+++ core/admin.py
@@ -30,10 +30,9 @@ class AppointmentsAdmin(admin.ModelAdmin):
     list_display = ['patient', 'doctor', 'appointment_date' ,'status']
     search_fields = ['patient__name', 'doctor__name']
 
+
 admin.site.register(models.Patient)
 admin.site.register(models.Doctor)
 admin.site.register(models.Appointment, AppointmentsAdmin)
 admin.site.register(models.Medication)
 admin.site.register(models.Prescription, PrescriptionAdmin)
-admin.site.register(models.PrescriptionMedication)
-admin.site.register(models.Billing)
